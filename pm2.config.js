3e920143378bb51287bcfaf816a5205d79631db9
liangjunren <liangjunren@bytedance.com>
2020-09-08 22:10:39 +0800

    支持简单聊

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,6 @@
 module.exports={
   "script": "ts-node", // or locally "./node_modules/.bin/_ts-node"
-  "args": "example/ding-dong-bot.ts",
+  "args": "examples/ding-dong-bot.ts",
   env: {
     WECHATY_PUPPET: 'wechaty-puppet-padplus',
     WECHATY_PUPPET_PADPLUS_TOKEN:'puppet_padplus_1493516d9d02fe28',
